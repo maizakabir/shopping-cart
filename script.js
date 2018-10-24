12f0b4643eaa191058bf5828812672aef394874d
Amirah Kabir <maiza1497@gmail.com>
2018-10-24 03:17:57 +0000

    Classwork 6 completed

--- script.js
+++ script.js
@@ -16,12 +16,52 @@ var cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart'
 localStorage.setItem('cart', JSON.stringify(cart));
 
 $(document).ready(function(){
-  // products.forEach(function(product, index){
-  //   //console.log(index);
-  //   var colDiv = $('div').addClass('col-md-4');
-  //   var cardDiv = $('div').addClass('card');
-  //   $("#product-row").prepend(colDiv);
-  // })
+  
+  products.forEach(function(product, index){
+    //console.log(index);
+    var colDiv = $('<div>').addClass('col-md-4');
+    var cardDiv = $('<div>').addClass('card');
+    
+    
+    var productImage = $('<img>').addClass("card-img-top");
+    productImage.attr('src', product.image);
+    cardDiv.append(productImage);
+    var cardBodyDiv = $('<div>').addClass("card-body");
+    cardDiv.append (cardBodyDiv);
+    var productTitle = $('<h5>').addClass ('card-title').text(product.name);
+    cardBodyDiv.append(productTitle);
+    var productPrice= $('<p>').addClass ('card-text').text ("$ "+ product.price);
+    cardBodyDiv.append(productPrice);
+    var addToCart = $('<button>').addClass ("btn btn-primary").text("Add to Cart").attr('id', index);
+    addToCart.click(function(event){
+      console.log(event.target.id);
+      var cartItem = products[event.target.id];
+      cartItem.quantity = 1;
+      cart.items.push(cartItem);
+      $("#itemNo").text(cart.items.length);
+      localStorage.setItem('cart', JSON.stringify(cart));
+    });
+    
+    cardBodyDiv.append(addToCart);
+    
+    colDiv.append(cardDiv);
+    $("#product-row").append(colDiv);
+    
+    
+  });
+  
+  
+  
+  // <div class="col-md-4">
+  //    <div class="card">
+  //        <img class="card-img-top" src="https://s3.amazonaws.com/mernbook/marketplace/tardis.png" alt="Card image cap">
+  //           <div class="card-body">
+  //             <h5 class="card-title">Tardis Figurine</h5>
+  //             <p class="card-text">$ 14</p>
+  //             <button class="btn btn-primary">Add to Cart</button>
+  //           </div>
+  //    </div>
+  // </div>
   
   // $("#itemNo").text(cart.items.length);
   
