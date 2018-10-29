3a6a8ca4beb104d44cbb820b5a26030afffc5973
Amirah Kabir <maiza1497@gmail.com>
2018-10-29 13:51:38 +0000

    classwork 6

--- script.js
+++ script.js
@@ -19,25 +19,26 @@ $(document).ready(function(){
   
   products.forEach(function(product, index){
     //console.log(index);
-    var colDiv = $('<div>').addClass('col-md-4');
-    var cardDiv = $('<div>').addClass('card');
-    var productImage = $('<img>').addClass("card-img-top");
+    var colDiv= $('<div>').addClass('col-md-4');
+    var cardDiv= $('<div>').addClass('card');
+    var productImage= $('<img>').addClass("card-img-top");
     productImage.attr('src', product.image);
     cardDiv.append(productImage);
-    var cardBodyDiv = $('<div>').addClass('card-body');
+    var cardBodyDiv= $('<div>').addClass('card-body');
     cardDiv.append (cardBodyDiv);
-    var productTitle = $('<h5>').addClass ('card-title').text(product.name);
+    var productTitle= $('<h5>').addClass ('card-title').text(product.name);
     cardBodyDiv.append(productTitle);
     var productPrice= $('<p>').addClass ('card-text').text ("$ "+ product.price);
     cardBodyDiv.append(productPrice);
-    var addToCart = $('<button>').addClass ("btn btn-primary").text("Add to Cart").attr('id', index);
+    var addToCart= $('<button>').addClass ("btn btn-primary").text("Add to Cart").attr('id', index);
     addToCart.click(function(event){
       console.log(event.target.id);
-      var cartItem = products[event.target.id];
-      cartItem.quantity = 1;
+      var cartItem= products[event.target.id];
+      cartItem.quantity= 1;
       cart.items.push(cartItem);
-      
+      console.log(cart.items.length);
       cart.total= cart.items.length * cartItem.price;
+      console.log(cart.items.length);
       $("#itemNo").text(cart.items.length);
       $("#total").text(cart.total);
       localStorage.setItem('cart', JSON.stringify(cart));
@@ -52,18 +53,15 @@ $(document).ready(function(){
   $("#total").text(cart.total);
     
   cart.items.forEach(function(item, index){
-    var colDiv = $('<div>').addClass('col-md-4');
-    var cardDiv = $('<div>').addClass('card');
-    var productImage = $('<img>').addClass("card-img-top");
+    var colDiv= $('<div>').addClass('col-md-4');
+    var cardDiv= $('<div>').addClass('card');
+    var productImage= $('<img>').addClass("card-img-top");
     productImage.attr('src', item.image);
     cardDiv.append(productImage);
-    
-    var cardBodyDiv = $('<div>').addClass('card-body');
+    var cardBodyDiv= $('<div>').addClass('card-body');
     cardDiv.append(cardBodyDiv);
-    
-    var productTitle = $('<h5>').addClass('card-title').text(item.name);
+    var productTitle= $('<h5>').addClass('card-title').text(item.name);
     cardBodyDiv.append(productTitle);
-    
     var productPrice= $('<p>').addClass('card-text').text ("$"+ item.price + " x ");
     cardBodyDiv.append(productPrice);
     
@@ -72,17 +70,15 @@ $(document).ready(function(){
     });
     productPrice.append(productQuantity);
     
-    var addToCart = $('<button>').addClass ("btn btn-primary").text("Add to Cart").attr('id', index);
+    var addToCart= $('<button>').addClass ("btn btn-primary").text("Add to Cart").attr('id', index);
     addToCart.click(function(event){
-      // console.log(item.price * item.quantity);
-      
       var i;
-      for(i = 0; i < item.quantity; i++){
-        var cartItem = products[event.target.id];
+      for(i= 0; i<item.quantity; i++){
+        var cartItem= products[event.target.id];
         cart.items.push(cartItem);
       }
+       cart.total= cart.total + (item.price * item.quantity);
       
-      cart.total = cart.total + (item.price * item.quantity);
       $("#itemNo").text(cart.items.length);
       $("#total").text(cart.total);
       
